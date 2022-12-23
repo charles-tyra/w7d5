26c73b1cd12cdfbc1423a219dcb231c965e3029b
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-23 11:40:46 -0500

    Users and Sessions controller creation - double checked through notes, will possibly change implementation once we get into working the project

--- practice-for-ch-rails-reddit-long-practice-main/Reddit/app/controllers/sessions_controller.rb
+++ practice-for-ch-rails-reddit-long-practice-main/Reddit/app/controllers/sessions_controller.rb
@@ -1,12 +1,30 @@
 class SessionsController < ApplicationController
-    def new
 
+    before_action :require_logged_out, only: [:new, :create]
+
+
+    def new
+        @user = User.new
+        render :new
     end 
 
     def create
+        username = params[:user][:username]
+        password = params[:user][:password]
+        @user = User.find_by_credentials(username, password)
+
+        if @user
+            log_in!(@user)
+            redirect_to user_url(@user)
+        else
+            redirect_to new_session_url
+        end
     end 
 
     def destroy
+        if logged_in?
+            log_out!
+        end
+        redirect_to users_url
     end 
-
 end
--- practice-for-ch-rails-reddit-long-practice-main/Reddit/app/controllers/users_controller.rb
+++ practice-for-ch-rails-reddit-long-practice-main/Reddit/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+    before_action :require_logged_out, only: [:create, :new]
+    before_action :require_logged_in, only: [:index, :show]
+
     def index
         @users = User.all
         render :index
