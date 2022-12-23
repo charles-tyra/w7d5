666392c671b035ee47d48cc2dfa08a379c944c26
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-23 11:22:45 -0500

    creation of UsersController, and addition of index route to User

--- practice-for-ch-rails-reddit-long-practice-main/Reddit/app/controllers/users_controller.rb
+++ practice-for-ch-rails-reddit-long-practice-main/Reddit/app/controllers/users_controller.rb
@@ -1,12 +1,33 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+        render :index
+    end
+
     def new
-        
+        @user = User.new
+        render :new
     end
 
     def create
-        
+        @user = User.new(user_params)
+
+        if @user.save
+            log_in!(user)
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = "Please provide a unique username and valid password, try again :)"
+            render :new
+        end
     end 
 
     def show
+        @user = User.find(params[:id])
+        render :show
     end 
+
+    private
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
 end
--- practice-for-ch-rails-reddit-long-practice-main/Reddit/config/routes.rb
+++ practice-for-ch-rails-reddit-long-practice-main/Reddit/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :index]
   resources :sessions, only: [:new, :create, :destroy]
 
 end
